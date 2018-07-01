2aed333f3ac69abd90620c5a02270321913476e7
Chedva Cohen <204043137@gov.il>
2018-07-01 10:27:16 +0300

    add field level

--- src/_Demo/App.js
+++ src/_Demo/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import RootStore from './rootStore';
-import LanguageStore from '../components/language/store'
-import ComponentsDemo from './ComponentsDemo';
-import {observer, Provider} from 'mobx-react';
+import React, { Component } from "react";
+import "./App.css";
+import RootStore from "./rootStore";
+import LanguageStore from "../components/language/store";
+import ComponentsDemo from "./ComponentsDemo";
+import { observer, Provider } from "mobx-react";
 
 @observer
-
 class App extends Component {
   render() {
-    const rootStore=new RootStore();
-    window.rootStore = rootStore
+    const rootStore = new RootStore();
+    window.rootStore = rootStore;
     const applicationData = {
       formLanguage: new LanguageStore()
-    }
-    window.applicationData = applicationData
-    console.log('applicationData',applicationData)
+    };
+    window.applicationData = applicationData;
+    console.log("applicationData", applicationData);
     return (
-      <Provider applicationData = {applicationData}>
+      <Provider applicationData={applicationData}>
         <div className="App">
-          <header >
-          </header>
-          <ComponentsDemo rootStore={rootStore}/>
+          <header />
+          <ComponentsDemo rootStore={rootStore} />
         </div>
       </Provider>
     );
--- src/_Demo/tabs/SimpleFieldsTab/SimpleFieldsTab.jsx
+++ src/_Demo/tabs/SimpleFieldsTab/SimpleFieldsTab.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import {observer} from 'mobx-react'
-import PersonalInformation from './containers/PersonalInformation/PersonalInformation'
-import control from '../../../Fields/hocs/control'
+import React from "react";
+import { observer } from "mobx-react";
+import PersonalInformation from "./containers/PersonalInformation/PersonalInformation";
 
 @observer
- class SimpleFieldsTab extends React.Component{
-    
-    constructor(props) {
-        super(props);
+class SimpleFieldsTab extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    }
-  
-    render(){
-         return(
-            <div>
-            <PersonalInformation userDetails={this.props.simpleFields.userDetails} />
-            <PersonalInformation userDetails={this.props.simpleFields.userDetails} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <PersonalInformation
+          userDetails={this.props.simpleFields.userDetails}
+        />
+      </div>
+    );
+  }
 }
-export default SimpleFieldsTab
+export default SimpleFieldsTab;
