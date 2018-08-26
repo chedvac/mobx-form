8c3160b9c7039c7f66e26d45af538a989db69bbd
Bella Wolpin <203884929@gov.il>
2018-08-26 09:28:26 +0300

    part

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,13 @@ module.exports = function override(config, env) {
   config = rewireMobX(config, env);
   config.resolve.alias._Demo = path.resolve('src/_Demo');
   config.resolve.alias.validations = path.resolve('src/validations/src');
-  config.resolve.alias.core = path.resolve('src/Mobx-MVVM/src');
+  config.resolve.alias.core = path.resolve('src/mobx-vm/src');
   config.resolve.alias.utils = path.resolve('src/utils/src');
+  config.resolve.alias['react-ui-components'] = path.resolve(
+    'src/react-ui-components/src'
+  );
+  config.resolve.alias['mobx-react-form'] = path.resolve(
+    'src/mobx-react-form/src'
+  );
   return config;
 };
