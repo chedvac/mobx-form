4f974a75900cbb9ff083a7d6f2b1373e16a061fb
chedva <chedva@MODIINE-043>
2018-10-04 09:12:32 +0300

    dialog

--- src/mobx-vm/src/modularViewModel.js
+++ src/mobx-vm/src/modularViewModel.js
@@ -9,142 +9,142 @@ import ValidateableDefinition from 'mobx-vm/validateableDefinition';
 import ModelMemberDefinition from 'mobx-vm/modelMemberDefinition';
 
 export default class ModularViewModel {
-  constructor(settings = {}) {
-    this.validateables = {};
-    this.modelMembers = {};
+    constructor(settings = {}) {
+        this.validateables = {};
+        this.modaelMembers = {};
 
-    this.validationsManager = new validationsManagerFactory(
-      settings.validations || []
-    );
-    this.validationState = observable(validationStateMultiMessages);
+        this.validationsManager = new validationsManagerFactory(
+            settings.validations || []
+        );
+        this.validationState = observable(validationStateMultiMessages);
 
-    forOwn(value => {
-      this.generateModelMember(value);
-    })(this._modelMembersSettings);
+        forOwn(value => {
+            this.generateModelMember(value);
+        })(this._modelMembersSettings);
 
-    forOwn(value => {
-      this.generateValidateable(value);
-    })(this._validateablesSettings);
-    this.validate = this.validate.bind(this);
-  }
-
-  generateModelMember(propertySettings) {
-    const modelMember = new ModelMemberDefinition(propertySettings);
-    this.modelMembers[modelMember.name] = modelMember;
-  }
-  generateValidateable(propertySettings) {
-    const validateable = new ValidateableDefinition(propertySettings);
-    this.validateables[validateable.name] = validateable;
-    reaction(
-      () => this[validateable.name],
-      value => validateable.validate(value)
-    );
-  }
-
-  /**     
-* @memberof ModularViewModel         
-* @function "getAction"
-* @description return action of property
-* @return {function} action
-* @example 
-  PersonalInfo.getAction();
-*/
-  getAction(name) {
-    return this[`set${upperFirst(name)}`];
-  }
-  @action
-  setValidationState(validationState) {
-    Object.assign(this.validationState, validationState);
-  }
-  /**     
-* @memberof ModularViewModel         
-* @function "validate"
-* @description validate modularViewModel and its properties
-* @return {bool} manipulation result
-* @example 
-  PersonalInfo.validate();
-*/
-  async validate() {
-    const validationResult = await this.validationsManager.validateMultiResults(
-      this
-    );
-    const modelValidationResult = await this.validateModel();
-    this.setValidationState({
-      messages: validationResult.messages,
-      isValid: modelValidationResult && validationResult.isValid
-    });
-    return this.validationState.isValid;
-  }
-
-  /**     
-  * @memberof ModularViewModel         
-  * @function "validateModel"
-  * @description validate all model properties
-  * @return {bool} properties validation state result
-  * @example 
-    PersonalInfo.validateModel();
-  */
-  async validateModel() {
-    let modelValid = true;
-    for (const property in this.modelMembers) {
-      if (this.modelMembers.hasOwnProperty(property)) {
-        const currentPropertyValid = await this._validateByType(property);
-        modelValid = modelValid && currentPropertyValid;
-      }
+        forOwn(value => {
+            this.generateValidateable(value);
+        })(this._validateablesSettings);
+        this.validate = this.validate.bind(this);
     }
-    return modelValid;
-  }
-  async _validateByType(name) {
-    const instance = this[name];
-    return (await instance) instanceof ModularViewModel
-      ? instance.validate()
-      : this.validateables[name].validate(this[name]);
-  }
-  /**     
-    * @memberof ModularViewModel         
-    * @function "addValidations"
-    * @description add Validations to existing property
-    * @param {string} propertyName propertyName
-    * @param {array} validations validations to add
-    * @return {void} 
-    * @example 
-      PersonalInfo.addValidations('firstName',[ maxlength({ value: 15 })]);
-    */
-  addValidations(propertyName, validations) {
-    this.validateables[propertyName].validationsManager.addValidations(
-      validations
-    );
-  }
-  /**     
-  * @memberof ModularViewModel         
-  * @function "reset"
-  * @description validate all model properties
-  * @return {bool} properties validation state result
-  * @example 
-    PersonalInfo.reset();
-  */
-  reset() {
-    Object.values(this.modelMembers).forEach(property => {
-      if (property.reset) {
-        property.reset();
-      }
-    });
-  }
 
-  /**     
-    * @memberof ModelMembersManager        
-    * @function "map"
-    * @description map all properties array
-    * @param {object} params
+    generateModelMember(propertySettings) {
+        const modelMember = new ModelMemberDefinition(propertySettings);
+        this.modelMembers[modelMember.name] = modelMember;
+    }
+    generateValidateable(propertySettings) {
+        const validateable = new ValidateableDefinition(propertySettings);
+        this.validateables[validateable.name] = validateable;
+        reaction(
+            () => this[validateable.name],
+            value => validateable.validate(value)
+        );
+    }
+
+    /**     
+  * @memberof ModularViewModel         
+  * @function "getAction"
+  * @description return action of property
+  * @return {function} action
+  * @example 
+    PersonalInfo.getAction();
+  */
+    getAction(name) {
+        return this[`set${upperFirst(name)}`];
+    }
+    @action
+    setValidationState(validationState) {
+        Object.assign(this.validationState, validationState);
+    }
+    /**     
+  * @memberof ModularViewModel         
+  * @function "validate"
+  * @description validate modularViewModel and its properties
+  * @return {bool} manipulation result
+  * @example 
+    PersonalInfo.validate();
+  */
+    async validate() {
+        const validationResult = await this.validationsManager.validateMultiResults(
+            this
+        );
+        const modelValidationResult = await this.validateModel();
+        this.setValidationState({
+            messages: validationResult.messages,
+            isValid: modelValidationResult && validationResult.isValid
+        });
+        return this.validationState.isValid;
+    }
+
+    /**     
+    * @memberof ModularViewModel         
+    * @function "validateModel"
+    * @description validate all model properties
+    * @return {bool} properties validation state result
     * @example 
-        modelMembersManager1.map(tab);
+      PersonalInfo.validateModel();
     */
-  @assertParametersType({ params: PropTypes.object })
-  map(params) {
-    Object.values(this.getProperties()).forEach(property => {
-      property.map(params);
-    });
-  }
+    async validateModel() {
+        let modelValid = true;
+        for (const property in this.modelMembers) {
+            if (this.modelMembers.hasOwnProperty(property)) {
+                const currentPropertyValid = await this._validateByType(property);
+                modelValid = modelValid && currentPropertyValid;
+            }
+        }
+        return modelValid;
+    }
+    async _validateByType(name) {
+        const instance = this[name];
+        return (await instance) instanceof ModularViewModel
+            ? instance.validate()
+            : this.validateables[name].validate(this[name]);
+    }
+    /**     
+      * @memberof ModularViewModel         
+      * @function "addValidations"
+      * @description add Validations to existing property
+      * @param {string} propertyName propertyName
+      * @param {array} validations validations to add
+      * @return {void} 
+      * @example 
+        PersonalInfo.addValidations('firstName',[ maxlength({ value: 15 })]);
+      */
+    addValidations(propertyName, validations) {
+        this.validateables[propertyName].validationsManager.addValidations(
+            validations
+        );
+    }
+    /**     
+    * @memberof ModularViewModel         
+    * @function "reset"
+    * @description validate all model properties
+    * @return {bool} properties validation state result
+    * @example 
+      PersonalInfo.reset();
+    */
+    reset() {
+        Object.values(this.modelMembers).forEach(property => {
+            if (property.reset) {
+                property.reset();
+            }
+        });
+    }
+
+    /**     
+      * @memberof ModelMembersManager        
+      * @function "map"
+      * @description map all properties array
+      * @param {object} params
+      * @example 
+          modelMembersManager1.map(tab);
+      */
+    @assertParametersType({ params: PropTypes.object })
+    map(params) {
+        Object.values(this.getProperties()).forEach(property => {
+            property.map(params);
+        });
+    }
 }
 /**     
 * @memberof ModularViewModel         
@@ -155,12 +155,12 @@ export default class ModularViewModel {
   PersonalInfo.setValidateableSettings({});
 */
 ModularViewModel.prototype.setValidateableSettings = assertParametersType(
-  { settings: PropTypes.shape({ name: PropTypes.string.isRequired }) },
-  function setValidateableSettings(settings) {
-    //'this'- every class that extends ModularViewModel
-    this._validateablesSettings = this._validateablesSettings || {};
-    this._validateablesSettings[settings.name] = settings;
-  }
+    { settings: PropTypes.shape({ name: PropTypes.string.isRequired }) },
+    function setValidateableSettings(settings) {
+        //'this'- every class that extends ModularViewModel
+        this._validateablesSettings = this._validateablesSettings || {};
+        this._validateablesSettings[settings.name] = settings;
+    }
 );
 /**     
 * @memberof ModularViewModel         
@@ -171,10 +171,10 @@ ModularViewModel.prototype.setValidateableSettings = assertParametersType(
   PersonalInfo.setModelMemberSettings({});
 */
 ModularViewModel.prototype.setModelMemberSettings = assertParametersType(
-  { settings: PropTypes.shape({ name: PropTypes.string.isRequired }) },
-  function setModelMemberSettings(settings) {
-    //'this'- every class that extends ModularViewModel
-    this._modelMembersSettings = this._modelMembersSettings || {};
-    this._modelMembersSettings[settings.name] = settings;
-  }
+    { settings: PropTypes.shape({ name: PropTypes.string.isRequired }) },
+    function setModelMemberSettings(settings) {
+        //'this'- every class that extends ModularViewModel
+        this._modelMembersSettings = this._modelMembersSettings || {};
+        this._modelMembersSettings[settings.name] = settings;
+    }
 );
